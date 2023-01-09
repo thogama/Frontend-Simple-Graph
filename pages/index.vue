6e689af117d55094a12da2f1c373760deb8c10d8
thogama <dethfiregrasp@gmail.com>
2023-01-08 21:43:50 -0300

    using websockets

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,29 @@
+<script lang="ts" setup>
+
+const state = useState("state", () => [
+    {
+        label: "Price 1",
+        backgroundColor: "#c3dd32",
+        data: []
+    },
+    
+])
+
+const ws = new WebSocket("ws://localhost:3333")
+ws.addEventListener("open", () => {
+    console.log("i have a connection bro")
+})
+ws.addEventListener("message", async (event) => {
+    //  console.log(JSON.parse(event['data']))
+    console.log(JSON.parse(event['data']))
+    //  console.log(state.value[0].data = event.data)
+    state.value[0].data = JSON.parse(event['data'])
+
+})
+useHead({
+    title: "Chart js"
+})
+</script>
 <template>
     <div class="m-2 border border-primary gx-5  row ">
 
@@ -15,40 +41,18 @@
             </div>
 
             <a class="btn rounded-pill  text-primary " href="https://github.com/thogama">
-                by Alan
+                by Alan 
             </a>
 
         </div>
-
         <div class="col-lg-9 ">
-            <!-- <BarChart  :colour=(color) :label=(label) :labels=(labels) :data=(data) />  -->
-            <LineChart :datasets="datasets" :labels=(labels) />
+            <LineChart ref="line" :datasets="state" :labels=([1,2,3,4,5]) />
 
         </div>
     </div>
 
-
 </template>
-
-<script setup>
-const datasets = [
-    {
-        label: "Price 1",
-        backgroundColor: "#c3dd32",
-        data: [20, 10, 50, 15, 50]
-    },
-    {
-        label: "Price 2",
-        backgroundColor: "#dd326d",
-        data: [10, 5, 25, 7.5, 25]
-    },
-]
-const labels = ["mes2", "mes3", "m32", "mes4", "mes5"]
-useHead({
-    title: "Chart js"
-})
-</script>
-
+ 
 <style scoped>
 
 </style>
